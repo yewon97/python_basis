a0fb3bbdf1f0162d62b6b1538542201588cea2eb
yewon97 <dpdnjs9936@gmail.com>
2025-01-15 00:01:41 +0900

    함수 매개변수 키워드 인자, 가변인자, 키워드 가변인자

--- 1-5/function2.py
+++ 1-5/function2.py
@@ -8,3 +8,33 @@ def welcome(city, name = "Guest", room = None):
 
 welcome("서울")
 welcome("서울", "연탄이")
+
+# 키워드 인자
+def display_info( age, city, name):
+  print(f"이름: {name}, 나이: {age}, 도시: {city}")
+
+display_info(name="연탄이", age=2, city="서울")
+# 키워드 인자인 경우 순서는 상관없음
+
+# 가변인자 리스트 (튜플)
+# 매개변수는 가변인자 앞으로 작성해야함
+# 만약 가변인자 뒤에 매개변수를 추가하고 싶으면 키워드 인자를 사용해야함
+def calc_sum(*args):
+  total = 0
+  for arg in args:
+    total += arg     # total = total + arg
+  return total
+
+print(calc_sum(1, 2, 3, 4, 5))
+print(calc_sum(10, 100))
+
+# 키워드 가변인자 리스트
+def print_info(**kwargs):
+  for key, value in kwargs.items():
+    print(f"{key}: {value}")
+
+print_info(name="연탄이", age=30, city="서울")
+
+info = {"name": "연탄이", "age": 30, "city": "서울"}
+# 딕셔너리를 키워드 인자로 전달하기 위해서는 **를 사용해야함
+print_info(**info)
